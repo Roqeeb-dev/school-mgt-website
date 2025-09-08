69adc5e5eb236d7fc309ad722ee5e5e89c99a8f2
Roqeeb <shafiriyuroqeeb@gmail.com>
2025-09-08 19:52:00 +0100

    added login page

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,11 +1,9 @@
 export default function Login() {
   return (
-    <main>
-      <div className="my-5">
-        <h1 className="text-4xl font-semibold">Welcome</h1>
-        <p className="text-gray-600 text-sm p-3">
-          Please login to your account
-        </p>
+    <main className="flex flex-col gap-5">
+      <div className="my-7">
+        <h1 className="text-4xl font-bold">Welcome</h1>
+        <p className="text-gray-600 text-sm">Please login to your account</p>
       </div>
       <form action="" className="flex flex-col gap-6">
         <input
@@ -13,21 +11,35 @@ export default function Login() {
           name="username"
           id="username"
           placeholder="Username or Email"
-          className="border-b focus:outline-none text-sm p-3"
+          className="border-b focus:outline-none text-sm px-2 py-4"
         />
         <input
           type="password"
           name="password"
           id="password"
           placeholder="Password"
-          className="border-b focus:outline-none text-sm p-3"
+          className="border-b focus:outline-none text-sm px-2 py-4"
         />
-        <div>
-          <a href="#">forgot password?</a>
-          <button>Login</button>
+        <div className="flex items-center justify-between">
+          <a
+            href="#"
+            className="text-sm text-gray-600 hover:text-black underline"
+          >
+            Forgot password?
+          </a>
+          <button className="bg-[#F2BA1D] font-semibold p-2 rounded-2xl text-sm w-[50%] text-center hover:bg-yellow-500">
+            Login
+          </button>
         </div>
-        <button>Register your school in our apps</button>
-        <a href="#">Terms and Conditions & Privacy Policy</a>
+        <button className="bg-gray-200 p-2 rounded-3xl text-sm transition-bg duration-300 hover:bg-black hover:text-white">
+          Register your School in Our Apps
+        </button>
+        <a
+          href="#"
+          className="text-center text-xs text-gray-600 hover:text-black"
+        >
+          Terms and Conditions & Privacy Policy
+        </a>
       </form>
     </main>
   );
--- components/Register.jsx
+++ components/Register.jsx
@@ -1,7 +1,38 @@
 export default function Register() {
   return (
-    <div>
-      <h1>Register page</h1>
-    </div>
+    <main className="flex flex-col gap-5">
+      <div className="my-7">
+        <h1 className="text-4xl font-bold">Welcome</h1>
+        <p className="text-gray-600 text-sm">Please register to your account</p>
+      </div>
+      <form action="" className="flex flex-col gap-6">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email address"
+          className="border-b focus:outline-none text-sm px-2 py-4"
+        />
+        <input
+          type="text"
+          name="organization-name"
+          id="organization-name"
+          placeholder="Enter your organization name"
+          className="border-b focus:outline-none text-sm px-1 py-4"
+        />
+        <input
+          type="text"
+          name="school-name"
+          id="school-name"
+          placeholder="Enter your School name"
+          className="border-b focus:outline-none text-sm px-1 py-4"
+        />
+        <div className="flex justify-end">
+          <button className="bg-[#F2BA1D] font-semibold p-2 rounded-2xl text-sm w-[50%] text-center hover:bg-yellow-500">
+            Register
+          </button>
+        </div>
+      </form>
+    </main>
   );
 }
