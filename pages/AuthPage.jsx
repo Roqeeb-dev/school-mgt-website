9bec9548f97c5e0b908940b319eed9d4facc2d81
Roqeeb <shafiriyuroqeeb@gmail.com>
2025-09-09 09:30:25 +0100

    styling login page

--- pages/AuthPage.jsx
+++ pages/AuthPage.jsx
@@ -6,43 +6,45 @@ export default function AuthPage() {
   const activeTab = location.pathname.includes("login") ? "Login" : "Register";
 
   return (
-    <section className="flex h-screen">
-      {/* Left side */}
-      <div className="flex-1 flex items-center justify-center bg-red-800">
-        <img src={logo} alt="Main logo" />
-      </div>
-
-      {/* Right side */}
-      <div className="bg-white w-[400px] p-5 lg:p-10 flex flex-col">
-        {/* Tabs */}
-        <div className="flex items-center justify-center rounded-2xl mb-4 bg-gray-50">
-          <Link
-            to="/auth/login"
-            className={`p-2 rounded-2xl text-sm w-[50%] text-center ${
-              activeTab === "Login"
-                ? "bg-[#F2BA1D] font-semibold"
-                : "bg-transparent"
-            }`}
-          >
-            Login
-          </Link>
-          <Link
-            to="/auth/register"
-            className={`p-2 rounded-2xl text-sm w-[50%] text-center ${
-              activeTab === "Register"
-                ? "bg-[#F2BA1D] font-semibold"
-                : "bg-transparent"
-            }`}
-          >
-            Register
-          </Link>
+    <main className="flex h-screen items-center justify-center">
+      <section className="flex w-[85%] h-[85vh] rounded-xl shadow-xl">
+        {/* Left side */}
+        <div className="flex-1 flex items-center justify-center bg-red-800">
+          <img src={logo} alt="Main logo" />
         </div>
 
-        {/* Dynamic content */}
-        <div>
-          <Outlet />
+        {/* Right side */}
+        <div className="bg-white w-[400px] p-5 lg:p-10 flex flex-col">
+          {/* Tabs */}
+          <div className="flex items-center justify-center rounded-2xl mb-4 bg-gray-50">
+            <Link
+              to="/auth/login"
+              className={`p-2 rounded-2xl text-sm w-[50%] text-center ${
+                activeTab === "Login"
+                  ? "bg-[#F2BA1D] font-semibold"
+                  : "bg-transparent"
+              }`}
+            >
+              Login
+            </Link>
+            <Link
+              to="/auth/register"
+              className={`p-2 rounded-2xl text-sm w-[50%] text-center ${
+                activeTab === "Register"
+                  ? "bg-[#F2BA1D] font-semibold"
+                  : "bg-transparent"
+              }`}
+            >
+              Register
+            </Link>
+          </div>
+
+          {/* Dynamic content */}
+          <div>
+            <Outlet />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 }
