1424304968ad95cffa52fb1ab16916ebc0b9423a
Roqeeb <shafiriyuroqeeb@gmail.com>
2025-09-10 21:07:53 +0100

    prelim styling of addon page

--- components/Addon.jsx
+++ components/Addon.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { X } from "lucide-react";
+
 export default function Addon() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Hello, i am addon services</h1>
-    </div>
+    <section>
+      <div className="flex items-center justify-between">
+        <div>
+          <p className="text-2xl font-semibold">Add-On Services</p>
+          <p className="text-[#F2BA1D] text-xs">To Get more features</p>
+        </div>
+        <div className="p-1 cursor-pointer rounded-full bg-black hover:bg-gray-800">
+          <X className="text-white h-3 w-3" />
+        </div>
+      </div>
+    </section>
   );
 }
