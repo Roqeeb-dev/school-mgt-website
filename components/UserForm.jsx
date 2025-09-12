ac81965a5818a1f6deab472ff2d8267a244aa4df
Roqeeb <shafiriyuroqeeb@gmail.com>
2025-09-12 18:25:39 +0100

    saved userdata in state

--- components/UserForm.jsx
+++ components/UserForm.jsx
@@ -1,4 +1,15 @@
+import userContext from "../context/userContext";
+import { useContext, useState } from "react";
+
 export default function UserForm() {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    role: "",
+    password: "",
+  });
   return (
     <form className="my-15 grid grid-cols-1 md:grid-cols-2 gap-5 w-full max-w-3xl px-4">
       {/* First name */}
@@ -7,6 +18,10 @@ export default function UserForm() {
         name="first-name"
         id="first-name"
         placeholder="First Name"
+        onChange={(e) => {
+          setUserData({ ...userData, firstName: e.target.value });
+        }}
+        value={userData.firstName}
         className="p-3 placeholder:text-gray-500 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 transition"
       />
 
@@ -16,6 +31,10 @@ export default function UserForm() {
         name="last-name"
         id="last-name"
         placeholder="Last Name"
+        onChange={(e) => {
+          setUserData({ ...userData, lastName: e.target.value });
+        }}
+        value={userData.lastName}
         className="p-3 placeholder:text-gray-500 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 transition"
       />
 
@@ -25,6 +44,10 @@ export default function UserForm() {
         name="email"
         id="email"
         placeholder="Email Address"
+        onChange={(e) => {
+          setUserData({ ...userData, email: e.target.value });
+        }}
+        value={userData.email}
         className="p-3 placeholder:text-gray-500 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 transition"
       />
 
@@ -34,6 +57,10 @@ export default function UserForm() {
         name="phone-number"
         id="phone-number"
         placeholder="Phone Number"
+        onChange={(e) => {
+          setUserData({ ...userData, phoneNumber: e.target.value });
+        }}
+        value={userData.phoneNumber}
         className="p-3 placeholder:text-gray-500 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 transition"
       />
 
@@ -43,6 +70,10 @@ export default function UserForm() {
         name="role"
         id="role"
         placeholder="Your Role"
+        onChange={(e) => {
+          setUserData({ ...userData, role: e.target.value });
+        }}
+        value={userData.role}
         className="p-3 placeholder:text-gray-500 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 transition"
       />
 
@@ -52,6 +83,10 @@ export default function UserForm() {
         name="password"
         id="password"
         placeholder="Password"
+        onChange={(e) => {
+          setUserData({ ...userData, password: e.target.value });
+        }}
+        value={userData.password}
         className="p-3 placeholder:text-gray-500 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 transition"
       />
     </form>
